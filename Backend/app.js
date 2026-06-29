05959f8e2d3af2239dc682ca0d7acfe20228a721
AbdulBasit247 <ab0565461@gmail.com>
2026-06-30 02:43:07 +0500

    Cors Setup

--- Backend/app.js
+++ Backend/app.js
@@ -2,15 +2,20 @@ import express from 'express';
 import connectDB from './db/db.js';
 import userRoutes from './routes/user.routes.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 connectDB();
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true,
+}));
 app.use(cookieParser());
 
-app.use('/users', userRoutes);
+app.use('/api/users', userRoutes);
 
 // app.get('/', (req, res) => {
 //     res.send('Hello World!');
